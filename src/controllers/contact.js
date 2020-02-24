2d4d048f0d4a27bd8309bd3681dae0e6d693d05b
121593 <44259105+121593@users.noreply.github.com>
2020-02-24 11:50:58 +0100

    feature(contact): Adds captcha. Makes use of svg-captcha

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -1,20 +1,40 @@
 const config = require('config');
 const mailgun = require('../resources/mailgun');
 
+const svgCaptcha = require('svg-captcha');
+
 const views = require('../views');
 
 const view = './src/views/contact.ejs';
 
-module.exports.read = async (params, meta) => {
+module.exports.read = async (params, meta, req) => {
+  const captcha = svgCaptcha.create();
+  req.session.captcha = captcha.text
+
   return views.render(view, {
     user: meta.user,
     id: params.id,
     errorMessage: null,
-    successMessage: null
+    successMessage: null,
+    captcha:captcha.data
   });
 };
 
 module.exports.create = async (params, meta, req, res) => {
+
+  if(!params.captcha || req.session.captcha !== params.captcha) {
+    const captcha = svgCaptcha.create();
+    req.session.captcha = captcha.text
+
+    return views.render(view, {
+      user: meta.user,
+      id: params.id,
+      errorMessage: 'Wrong captcha',
+      successMessage: null,
+      captcha:captcha.data
+    });
+  }
+
   const message = {
     from: `${params.name} <${params.email}>`,
     to: config.email.to,
